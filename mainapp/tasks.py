20d891a1ca14be7be8b788470f2557e5df4244f2
AMEERAZAM08 <ameerazam08@gmail.com>
2022-09-08 11:38:33 +0530

    okay

--- mainapp/tasks.py
+++ mainapp/tasks.py
@@ -1,8 +1,8 @@
 from celery import shared_task
-
+from django.http.response import HttpResponse
+import  time
 @shared_task(bind=True)
 def test_func(self):
     #operations
-    for i in range(10):
-        print(i)
-    return "Done"
+    time.sleep(10)
+    return HttpResponse("Done")
